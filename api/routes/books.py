2c45e8f908799e342d6cd843719102ba0aadaf17
Jesse-maurice iyoha <iyohajessy@gmail.com>
2025-02-11 00:35:00 +0100

    genre issue fixed

--- api/routes/books.py
+++ api/routes/books.py
@@ -10,7 +10,7 @@ router = APIRouter()
 db = InMemoryDB()
 
 # Seed the database with initial data
-db.add_book(Book(id=1, title="The Hobbit", author="J.R.R. Tolkien", publication_year=1937, genre=Genre.FANTASY))
+db.add_book(Book(id=1, title="The Hobbit", author="J.R.R. Tolkien", publication_year=1937, genre=Genre.SCI_FI))
 db.add_book(Book(id=2, title="The Lord of the Rings", author="J.R.R. Tolkien", publication_year=1954, genre=Genre.FANTASY))
 db.add_book(Book(id=3, title="The Return of the King", author="J.R.R. Tolkien", publication_year=1955, genre=Genre.FANTASY))
 
